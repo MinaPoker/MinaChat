739b74a3937886ef0c2ba6872834dcb20f08a4be
VishalPachpor <Vishalpatil080502@gmail.com>
2024-01-23 00:49:07 +0530

    Chat input component created with emoji picker

---
+++ Client/src/components/ChatInput.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import Picker from "emoji-picker-react";
+import { IoMdSend } from "react-icons/io";
+import { BsEmojiSmileFill } from "react-icons/bs";
+
+const ChatInput = ({ handleSendMsg }) => {
+  const [msg, setMsg] = useState("");
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const handleEmojiPickerhideShow = () => {
+    setShowEmojiPicker(!showEmojiPicker);
+  };
+
+  const handleEmojiClick = (event, emojiObject) => {
+    let message = msg;
+    message += emojiObject.emoji;
+    setMsg(message);
+  };
+
+  const sendChat = (e) => {
+    e.preventDefault();
+    if (msg.length > 0) {
+      handleSendMsg(msg);
+      setMsg("");
+    }
+  };
+
+  return (
+    <div className="grid grid-col-[5%,95%] items-center py-1 pb-2">
+      <div className="flex items-center text-white gap-4">
+        <div className=" relative">
+          <BsEmojiSmileFill
+            className="text-lg cursor-pointer text-[#ffff00c8]"
+            onClick={handleEmojiPickerhideShow}
+          />
+          {showEmojiPicker && (
+            <Picker
+              className="absolute top-[-350px] bg-[#080420] border-[#9a86f3] shadow-md"
+              onEmojiClick={handleEmojiClick}
+              pickerStyle={pickerStyle}
+              emojiScrollWrapperStyle={{
+                "&::-webkit-scrollbar": {
+                  backgroundColor: "#080420",
+                  width: "5px",
+                },
+                "&::-webkit-scrollbar-thumb": {
+                  backgroundColor: "#9a86f3",
+                },
+              }}
+              emojiCategoriesStyle={{
+                button: {
+                  filter: "contrast(0)",
+                },
+              }}
+              emojiSearchStyle={{
+                backgroundColor: "transparent",
+                borderColor: "#9a86f3",
+              }}
+              emojiGroupStyle={{
+                ":before": {
+                  backgroundColor: "#080420",
+                },
+              }}
+            />
+          )}
+        </div>
+      </div>
+      <form
+        className="w-screen rounded-full flex items-center gap-8 bg-[#ffffff34]"
+        onSubmit={(e) => sendChat(e)}
+      >
+        <input
+          type="text"
+          placeholder="Type your message here"
+          value={msg}
+          onChange={(e) => setMsg(e.target.value)}
+          className="w-[90%] bg-transparent text-white border-none pl-4 text-lg selection:bg-[#9186f3] focus:outline-none"
+        />
+        <button className="p-[0.2rem] px-[2rem] rounded-full flex items-center justify-center bg-[#9186f3] border-none">
+          <IoMdSend className="text-2xl text-white" />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ChatInput;
